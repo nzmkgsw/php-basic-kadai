8ed68086440d8f2b1e628f9414a443c247dd0522
nzmkgsw <143275192+nzmkgsw@users.noreply.github.com>
2023-10-27 22:57:55 +0900

    kadai_sortフォルダをアップロード

---
+++ kadai_sort/sort.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎編_ソートの課題</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    function sort_2way($array, $order) {
+      if ($order === TRUE) {
+        // 昇順に並び替える
+        echo '昇順にソートします。<br>';
+        sort($array);
+        foreach ($array as $array) {
+          echo $array . '<br>';
+        }
+      } else {
+        // 降順に並び替える
+        echo '降順にソートします。<br>';
+        rsort($array);
+        foreach ($array as $array) {
+          echo $array . '<br>';
+        }
+      }
+    }
+
+    // ソートする配列を宣言
+    $num = [15, 4, 18, 23,10];
+
+    // 関数の呼び出し（昇順）
+    sort_2way($num, TRUE);
+
+    // 関数の呼び出し（降順）
+    sort_2way($num, FALSE);
+    ?>
+  </p>
+</body>
+</html>
